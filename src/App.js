2dfa020c997625db545fb00c428b838e5227eec7
Wallace Oliveira <96851872+Wallace-Tavares-Oliveira@users.noreply.github.com>
2022-05-21 23:07:41 -0300

    data fetching initiating

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Cards } from './components/Cards/Cards';
+import { Filters } from './components/Filters/Filters';
+
 
 function App() {
+
+  let [pageNumber, setPageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=2${pageNumber}`;
+
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(api).then(res=>res.json());
+      console.log(data.results);
+    })()  
+  },[api])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="text-center ubuntu my-5"><span className='text-primary'>Wallace Oliveira</span> portifolio</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+
+          <div className="col-8">
+            <div className="row">
+                <Cards />
+                <Cards />
+                <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
   );
 }
 
